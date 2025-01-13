a7c5080b4fcea05f79c8b72e4669d6f33616c905
Debarup <87480898+debarup24@users.noreply.github.com>
2025-01-13 12:00:32 +0530

    v 1.1.1

--- src/app.js
+++ src/app.js
@@ -2,9 +2,29 @@ const express = require("express") ;
 
 const app = express () ;
 
-app.use((req, res) => {
-    res.send("Hello fro the server side !") ; // req handler
-})   ;
+app.get("/user", (req,res) => {
+    res.send({firstName: "Dev", lastName: "Sarkar" })
+})
+
+app.post("/user", async (req, res) => {
+    console.log (req.body) ;
+    res.send("Data Successfully saved to database!") ; 
+})
+
+app.use("/" , (err,req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong!") ;
+    }
+         
+     }) ;
+
+// app.use("/home" , (req, res) => {
+//     res.send("Welcome to backend - server side !") ; // req handler
+// }) ;
+
+// app.use("/greeting" , (req, res) => {
+//     res.send("Namaste from the server !") ; 
+// })   ;
 
 app.listen(2421, () => {
     console.log ("Server is successfully listening on port 2421") ;
